03d89ca5d848a69ecba3cd296fb649a4b3193dbf
Chris Kreager <chrisservice@outlook.com>
2018-09-07 17:54:09 +0200

    Duleek

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,8 @@
 
 ActiveRecord::Schema.define(version: 2018_09_06_223857) do
 
-  create_table "administrators", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "remember_token"
-    t.datetime "remember_token_expires_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "heritages", force: :cascade do |t|
     t.string "title"
