5202b98b1f9f5cbda0792902e221677a1ba26171
Chris Kreager <chrisservice@outlook.com>
2018-09-11 16:35:10 +0200

    api

---
+++ app/controllers/Api/V1/heritages_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::HeritagesController < ApplicationController
+	def index
+		heritages = Heritage.all
+		render json: heritages
+	end
+
+	def show
+		heritage = Heritage.find(params[:id])
+		render json: heritage
+	end
+end
